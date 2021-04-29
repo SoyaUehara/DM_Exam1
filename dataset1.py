f1b828aa07417af399fa0f80ec018391dbd9c685
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-04-30 01:16:36 +0900

    exam1.3 first commit

--- dataset1.py
+++ dataset1.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import pandas as pd
+import matplotlib
+import matplotlib.font_manager as font_manager
 
 
 def true_function(x):
@@ -13,23 +15,27 @@ def true_function(x):
 
 
 if __name__ == "__main__":
+    font_path = '/Library/Fonts/Arial Unicode.ttf'
+    font_prop = font_manager.FontProperties(fname = font_path)
+    matplotlib.rcParams['font.family'] = font_prop.get_name()
     seed_List = []
     ture_num = []
     for i in range(0, 20):
         num = random.uniform(-1, 1)
         seed_List.append(num)
-        ture_num.append(true_function(num))
+        Gauss = np.random.normal(0.0, 2.0, (1, 1))
+        ture_num.append(true_function(num)+Gauss[0][0])
 
-    dic_arr = {'観測点': seed_List, '真値': ture_num}
+    dic_arr = {'観測点': seed_List, '観測値': ture_num}
     print(pd.DataFrame(dic_arr))
     dic_arr = pd.DataFrame(dic_arr)
     x = dic_arr['観測点']
-    y = dic_arr['真値']
+    y = dic_arr['観測値']
 
     fig = plt.figure()
     plt.scatter(x, y, color="black", label="data", s=5)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel('観測点')
+    plt.ylabel('観測値')
     plt.legend()
     plt.show()
-    fig.savefig("ex1.2.png")
+    fig.savefig("ex1.3.png")
