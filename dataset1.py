92130028f46b278a86687d53091041547043c996
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-04-29 23:34:17 +0900

    exam1.2 first commit

--- dataset1.py
+++ dataset1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+import pandas as pd
 
 
 def true_function(x):
@@ -11,13 +13,23 @@ def true_function(x):
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    datasets = np.array([-1, 1])
+    seed_List = []
+    ture_num = []
+    for i in range(0, 20):
+        num = random.uniform(-1, 1)
+        seed_List.append(num)
+        ture_num.append(true_function(num))
+
+    dic_arr = {'観測点': seed_List, '真値': ture_num}
+    print(pd.DataFrame(dic_arr))
+    dic_arr = pd.DataFrame(dic_arr)
+    x = dic_arr['観測点']
+    y = dic_arr['真値']
+
     fig = plt.figure()
-    plt.plot(datasets, true_function(datasets), color='black', label="data test")
+    plt.scatter(x, y, color="black", label="data", s=5)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
     plt.show()
-    fig.savefig("ex1.1.png")
+    fig.savefig("ex1.2.png")
