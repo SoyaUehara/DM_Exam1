2743b3af4bda62bf42dafdb625a3f3c5b772eadb
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-05-06 14:50:34 +0900

    exam1.8 first commit

--- learn1.py
+++ learn1.py
@@ -1,5 +1,7 @@
 import dataset1
+from sklearn import linear_model
 
 if __name__ == "__main__":
     df = dataset1.exam1_5('output.tsv')
-    print(df)
+    reg = linear_model.LinearRegression()
+    
